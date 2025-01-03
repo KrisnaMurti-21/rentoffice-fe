e7807510af07b0649b594cf985daebea9b5af987
KrisnaMurti-21 <krisnamr27@gmail.com>
2025-01-03 21:05:31 +0700

    add router dom and customized city detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import BookOffice from "./pages/BookOffice";
 import Browse from "./pages/Browse";
+import CheckBooking from "./pages/CheckBooking";
 import CityDetails from "./pages/CityDetails";
 import Details from "./pages/Details";
-import BookOffice from "./pages/BookOffice";
 import SuccessBooking from "./pages/SuccessBooking";
-import CheckBooking from "./pages/CheckBooking";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Browse />} />
-        <Route path="/offices/:slug" element={<Details />} />
-        <Route path="/offices/:slug/book" element={<BookOffice />} />
+        <Route path="/office/:slug" element={<Details />} />
+        <Route path="/office/:slug/book" element={<BookOffice />} />
         <Route path="/city/:slug" element={<CityDetails />} />
         <Route path="/success-booking" element={<SuccessBooking />} />
         <Route path="/check-booking" element={<CheckBooking />} />
